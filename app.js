53c53ba0e75c7ee145b07ae53fc81fe17ece97fa
Mikael Wallin <mikael.wallin@gmail.com>
2016-08-02 09:39:50 +0200

    Done with initial structure and style

--- app.js
+++ app.js
@@ -1,58 +1,145 @@
 "use strict"
 
 
-// ******************************************
-// Globals and require
+// *****************************************************************************
+// *****************************************************************************
+// Requires and basics
 
 const express = require( "express" )
 const app = express()
 
+const bodyParser = require( "body-parser" )
 
 
 
-// ******************************************
+
+// *****************************************************************************
+// *****************************************************************************
 // App setup
 
 app.set( "view engine", "ejs" );
 
+app.use( bodyParser.urlencoded({extended: true}) )
 
-// ******************************************
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Globals
+
+const campgrounds = [
+  {
+    name : "Salmon Creek",
+    image: "https://msdnshared.blob.core.windows.net/media/MSDNBlogsFS/prod.evol.blogs.msdn.com/CommunityServer.Blogs.Components.WeblogFiles/00/00/01/21/36/2818.IMG_2641.JPG"
+  },
+  {
+    name : "Dismals Canyon",
+    image: "http://dismalscanyon.com/campsites/images/caveman_5124_900px.jpg"
+  },
+  {
+    name : "Beachside",
+    image: "http://www.knightsnotes.com/Hawaii%20Trip/Campsite%20Near%20Beach.jpg"
+  },
+  {
+    name : "Salmon Creek",
+    image: "https://msdnshared.blob.core.windows.net/media/MSDNBlogsFS/prod.evol.blogs.msdn.com/CommunityServer.Blogs.Components.WeblogFiles/00/00/01/21/36/2818.IMG_2641.JPG"
+  },
+  {
+    name : "Dismals Canyon",
+    image: "http://dismalscanyon.com/campsites/images/caveman_5124_900px.jpg"
+  },
+  {
+    name : "Beachside",
+    image: "http://www.knightsnotes.com/Hawaii%20Trip/Campsite%20Near%20Beach.jpg"
+  },
+  {
+    name : "Salmon Creek",
+    image: "https://msdnshared.blob.core.windows.net/media/MSDNBlogsFS/prod.evol.blogs.msdn.com/CommunityServer.Blogs.Components.WeblogFiles/00/00/01/21/36/2818.IMG_2641.JPG"
+  },
+  {
+    name : "Dismals Canyon",
+    image: "http://dismalscanyon.com/campsites/images/caveman_5124_900px.jpg"
+  },
+  {
+    name : "Beachside",
+    image: "http://www.knightsnotes.com/Hawaii%20Trip/Campsite%20Near%20Beach.jpg"
+  },
+  {
+    name : "Salmon Creek",
+    image: "https://msdnshared.blob.core.windows.net/media/MSDNBlogsFS/prod.evol.blogs.msdn.com/CommunityServer.Blogs.Components.WeblogFiles/00/00/01/21/36/2818.IMG_2641.JPG"
+  },
+  {
+    name : "Dismals Canyon",
+    image: "http://dismalscanyon.com/campsites/images/caveman_5124_900px.jpg"
+  },
+  {
+    name : "Beachside",
+    image: "http://www.knightsnotes.com/Hawaii%20Trip/Campsite%20Near%20Beach.jpg"
+  },
+]
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
 // Routes
 
 app.get( "/", function ( req, res ) {
 
   res.render( "landing" )
 
+
 })
 
 
-app.get( "/campgrounds", function ( req, res ) {
 
-  const campgrounds = [
-    {
-      name : "Salmon Creek",
-      image: "https://msdnshared.blob.core.windows.net/media/MSDNBlogsFS/prod.evol.blogs.msdn.com/CommunityServer.Blogs.Components.WeblogFiles/00/00/01/21/36/2818.IMG_2641.JPG"
-    },
-    {
-      name : "Dismals Canyon",
-      image: "http://dismalscanyon.com/campsites/images/caveman_5124_900px.jpg"
-    },
-    {
-      name : "Adventurers Campsite",
-      image: "http://vignette1.wikia.nocookie.net/elderscrolls/images/b/b1/Adventurers_Campsite.png/revision/latest?cb=20121123194546"
-    },
-  ]
+
+app.get( "/campgrounds", function ( req, res ) {
 
   res.render( "campgrounds", {
     campgrounds: campgrounds
   })
 
+
 })
 
 
 
 
-// ******************************************
+app.post( "/campgrounds", function ( req, res ) {
+
+  // Make an object from the request
+  const newCampground = {
+    name : req.body.campName,
+    image: req.body.campImage
+  }
+
+  // Add campground to campgrounds-array
+  campgrounds.push( newCampground )
+
+  // Send user to the campg site
+  res.redirect( "/campgrounds" )
+
+
+})
+
+
+
+
+app.get( "/campgrounds/new", function ( req, res ) {
+
+  res.render( "newCamp" )
+
+
+})
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
 // Server
 
 app.listen( 3000, function () {
