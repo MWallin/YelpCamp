679fbdc1809670cd6fe5b8f47c4e6e4aa1bd780f
Mikael Wallin <mikael.wallin@gmail.com>
2016-08-13 16:20:25 +0200

    Added typings and som packages

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,4 +2,6 @@
 /// <reference path="globals/express/index.d.ts" />
 /// <reference path="globals/mongoose/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/passport-local/index.d.ts" />
+/// <reference path="globals/passport/index.d.ts" />
 /// <reference path="globals/serve-static/index.d.ts" />
